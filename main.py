1b46b301390dbad2433a39b8442ac5a56c2c1d47
KseniyaShestakova <91065721+KseniyaShestakova@users.noreply.github.com>
2022-04-15 11:42:01 +0300

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,5 @@ import sys
 # ожидается ввод типа <name of the file> <path_to_parameters> <path_to_nodes>
 # вязанные файлы, указанные в импортах сверху, должны лежать в той же папке
 parser = Parser()
-print(sys.argv[1])
-print(sys.argv[2])
 state = parser.parse(sys.argv[1], sys.argv[2])
 state.start()
